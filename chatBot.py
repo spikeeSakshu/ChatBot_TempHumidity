ec29eacf4f95dacd6c65bc00c47da28110764758
spikeeSakshu <spikee.sakshu@gmail.com>
2019-02-19 22:37:48 +0530

    Done

--- chatBot.py
+++ chatBot.py
@@ -7,6 +7,12 @@ Created on Tue Feb 19 20:49:33 2019
 
 
 import random
+import requests
+import json
+
+API_KEY="Your API KEY"
+API_EndPoint="https://api.openweathermap.org/data/2.5/weather?zip=122413,in&APPID="
+
 
 greeting_inputs=('hello','hi','greetings','sup',"what's up",'hey',)
 greeting_responses=['hi','hey','*nods*','hi there','hello','I am glad! You are talking to me']
@@ -18,19 +24,27 @@ def greeting(sentence):
             return random.choice(greeting_responses)
 
 
+def response():
+    call=requests.get(API_EndPoint+API_KEY)
+    
+    data=json.loads(call.text)    
+    temp=data['main']['temp']-273
+    humidity=data['main']['humidity']
+    print("Sure. The temp is ",round(temp,2),"Celcius","and the humidity is ",humidity,"%")
+    
+    
+    return temp
 def start():
     flag=True
     
     print("Welcome to Sakshu's ChatBot .....")
     
     while flag==True:
-        user_response=input("...")
+        user_response=input("... ")
         user_response=user_response.lower()
         
         if user_response!='bye':
-            if (user_response is 'Thanks' or user_response is 'Thank you'):
-                flag =False
-                
+            if (user_response is 'thanks' or user_response is 'thank you'):
                 print('Welcome')
                 
             else:
@@ -38,12 +52,14 @@ def start():
                     print('ROBO:'+greeting(user_response))
                     
                 else:
-                    print("ROBO: ",end=" ")
-                    print(responses(user_response))
-                    sent_tokens.remove(user_response)
-            
+                    print("ROBO:",end=" ")
+                    response()
+                    
         else:
             flag=False
             print('ROBO: BYE!')
             
 start()
+
+
+
